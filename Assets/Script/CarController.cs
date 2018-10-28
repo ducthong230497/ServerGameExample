b6a8a87c98ba777feaf4fb29cd7e00692105af0b
ducthong230497 <15520856@gm.uit.edu.vn>
2018-10-28 22:00:14 +0700

    set up GameScene a bit

---
+++ Assets/Script/CarController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+    [SerializeField] private int pushForce = 10;
+
+    private Rigidbody2D rigidbody2D;
+	// Use this for initialization
+	void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody2D.rotation++;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody2D.rotation--;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.AddForce(Vector3.up * pushForce * Time.deltaTime);
+    }
+}
